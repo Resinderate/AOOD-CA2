1d74641b454de497c05894ef3a2019aa51b76019
Ronan Murphy <ronan_murfy@hotmail.com>
2013-04-24 20:03:55 +0100

    finished main menu

--- MainApp.java
+++ MainApp.java
@@ -24,14 +24,72 @@ public class MainApp {
         
         System.out.println(":::::::::::::::::::::::::::::::");
         System.out.println("Welcome to the database system!");
-        System.out.println(":::::::::::::::::::::::::::::::");
         
-        printMainMenu();
         
+        int choice = 0;
+        while(choice != 4)
+        {        
+            printMainMenu();
+            choice = sc.nextInt("\nEnter your choice: ", 1, 4);
+
+            if(choice == 1)
+            {
+                
+                String fileName = sc.nextLine("Enter a name for your new database file (eg. something.db)", 3, 50);
+                boolean valid = db.create(fileName);
+                while(!valid)
+                {
+                    fileName = sc.nextLine("That file already exists, please enter another", 3, 50);
+                    valid = db.create(fileName);
+                }
+            }
+            else if(choice == 2)
+            {
+                if(!db.isEmpty())
+                {
+                    System.out.println("Here is a list of existing files: ");
+                    db.printFileNames();
+                    String fileName = sc.nextLine("\nEnter the name of a valid database to open: ", 3, 50);
+                    boolean valid = db.open(fileName);
+                    while(!valid)
+                    {
+                        fileName = sc.nextLine("That filename does not exist, please try another.", 3, 50);
+                        valid = db.create(fileName);
+                    }
+
+                    //openFileMenu                                                                                                                  <<<<<<<<<<<<<<<<<<<<<<<<
+                }
+                else
+                {
+                    System.out.println("There is currently no existing database files.");
+                }
+            }
+            else if(choice == 3)
+            {
+                if(!db.isEmpty())
+                {
+                    System.out.println("Here is a list of existing files: ");
+                    db.printFileNames();
+                    String fileName = sc.nextLine("\nEnter the name of a valid database to delete: ", 3, 50);
+                    boolean valid = db.delete(fileName);
+                    while(!valid)
+                    {
+                        fileName = sc.nextLine("That filename does not exist, please try another.", 3, 50);
+                        valid = db.delete(fileName);
+                    }
+                }
+                else
+                {
+                    System.out.println("There is currently no existing database files.");
+                }
+            }
+            
+        }
     }
     
     public void printMainMenu()
     {
+        System.out.println(":::::::::::::::::::::::::::::::");
         System.out.println("Press 1 to Create a new database");
         System.out.println("Press 2 to Open an existing database");
         System.out.println("Press 3 to Delete an existing database");
